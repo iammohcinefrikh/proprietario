b56e5ca66e0e4ccf2fe90b27781c292330c38126
Mohcine Frikh <iammohcinefrikh@hotmail.com>
2024-09-25 23:26:18 +0100

    made few import updates again

--- client/app/login/form.tsx
+++ client/app/login/form.tsx
@@ -7,8 +7,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../../components/ui/form";
 import { Alert, AlertDescription, AlertTitle } from "../../components/ui/alert";
 
